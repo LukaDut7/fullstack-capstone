43edf0825fe2b922dbd317214142748352250fbb
Luka <luka>
2024-10-14 15:24:56 -0400

    auth commit

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -8,21 +8,21 @@ const pino = require('pino');  // Import Pino logger
 dotenv.config();
 
 const logger = pino();  // Create a Pino logger instance
-
+const { body, validationResult } = require('express-validator');
 //Create JWT secret
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-      //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
-      const db = await connectToDatabase();
+        //Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`.
+        const db = await connectToDatabase();
 
-      //Access the `users` collection
-      const collection = db.collection("users");
+        //Access the `users` collection
+        const collection = db.collection("users");
 
-      //Check for existing email in DB
-      const existingEmail = await collection.findOne({ email: req.body.email });
+        //Check for existing email in DB
+        const existingEmail = await collection.findOne({ email: req.body.email });
 
         if (existingEmail) {
             logger.error('Email id already exists');
@@ -31,7 +31,7 @@ router.post('/register', async (req, res) => {
 
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
-        const email=req.body.email;
+        const email = req.body.email;
 
         //Save user details
         const newUser = await collection.insertOne({
@@ -51,14 +51,14 @@ router.post('/register', async (req, res) => {
         //Create JWT
         const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
-        res.json({ authtoken,email });
+        res.json({ authtoken, email });
     } catch (e) {
         logger.error(e);
         return res.status(500).send('Internal server error');
     }
 });
 
-    //Login Endpoint
+//Login Endpoint
 router.post('/login', async (req, res) => {
     console.log("\n\n Inside login")
 
@@ -70,7 +70,7 @@ router.post('/login', async (req, res) => {
 
         if (theUser) {
             let result = await bcryptjs.compare(req.body.password, theUser.password)
-            if(!result) {
+            if (!result) {
                 logger.error('Passwords do not match');
                 return res.status(404).json({ error: 'Wrong pasword' });
             }
@@ -93,7 +93,52 @@ router.post('/login', async (req, res) => {
     } catch (e) {
         logger.error(e);
         return res.status(500).json({ error: 'Internal server error', details: e.message });
-      }
+    }
+});
+
+// {Insert it along with other imports} Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+
+router.put('/update', async (req, res) => {
+    // Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        // Task 5: find user credentials in database
+        const existingUser = await collection.findOne({ email });
+        existingUser.updatedAt = new Date();
+
+        // Task 6: update user credentials in database
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        // Task 7: create JWT authentication using secret key from .env file
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({ authtoken });
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+
+    }
 });
 
 module.exports = router;
