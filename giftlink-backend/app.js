cdd22721ad91cc39f2aaa0f8e0125d937481cedf
Luka <luka>
2024-10-14 10:49:08 -0400

    searchAPI commit

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -42,7 +42,7 @@ app.use('./api/gifts' , giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 const searchRoutes = require('./routes/searchRoutes');
-app.use('./api/gifts' , searchRoutes);
+app.use('./api/search' , searchRoutes);
 // Global Error Handler
 app.use((err, req, res, next) => {
     console.error(err);
